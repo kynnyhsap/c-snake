655cf018a0f53be5a91c4a1abfc02716c21240fb
Andrew Pashinnik <tobirawork@gmail.com>
2018-11-07 22:44:26 +0200

    feat(main): add basic intital logic

--- src/header.h
+++ src/header.h
@@ -1,5 +1,10 @@
 enum Directoins { UP, DOWN, LEFT, RIGHT };
 
+#define BUTTON_UP 'w'
+#define BUTTON_DOWN 's'
+#define BUTTON_LEFT 'a'
+#define BUTTON_RIGHT 'd'
+
 typedef struct {
     unsigned int R;
     unsigned int G;
@@ -53,7 +58,7 @@ void Snake_changeDirection(Snake *snake, enum Directoins newDirection);
 void Snake_move(Snake *snake, Apple *apple);
 void Snake_tryEatApple(Snake *snake, Apple *apple);
 
-BodyPart Body_createPart(enum Directoins direction, Point2D location, Color color);
+BodyPart Body_createPart(int bodyLength, enum Directoins direction, Point2D location);
 void Body_push(Body *body, BodyPart newPart);
 
 Apple Apple_generate(unsigned int maxX, unsigned int maxY);
@@ -61,3 +66,7 @@ void Apple_render(Apple apple);
 
 void ChangePoints_push(ChangePointsList *changePoints, ChangePoint newPoint);
 void ChangePoints_shift(ChangePointsList *changePoints);
+
+extern const Color COLOR_GREEN;
+extern const Color COLOR_GREEN_LIGHT;
+extern const Color COLOR_GREEN_DARK;
--- src/main.c
+++ src/main.c
@@ -1,5 +1,93 @@
+#include <progbase/canvas.h>
+#include <progbase/console.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "header.h"
 
-int main() { return 0; }
+const Color COLOR_GREEN = {
+    .R = 102,
+    .G = 204,
+    .B = 0,
+};
+
+const Color COLOR_GREEN_DARK = {
+    .R = 0,
+    .G = 153,
+    .B = 0,
+};
+
+const Color COLOR_GREEN_LIGHT = {
+    .R = 153,
+    .G = 255,
+    .B = 51,
+};
+
+void render(Snake snake, Apple apple) {
+    Canvas_beginDraw();
+    Snake_render(snake);
+    Apple_render(apple);
+    Canvas_endDraw();
+}
+
+int main() {
+    srand(time(NULL));
+
+    struct ConsoleSize consoleSize = Console_size();
+    const int canvasWidth          = consoleSize.columns;
+    const int canvasHeight         = consoleSize.rows * 2;
+
+    Console_clear();
+    Canvas_setSize(canvasWidth, canvasHeight);
+    Canvas_invertYOrientation();
+
+    Snake snake = {
+        .score     = 0,
+        .direction = RIGHT,
+        .body      = {.length = 0},
+    };
+
+    snake.body.parts = (BodyPart*)malloc(4 * sizeof(BodyPart));
+
+    for (int i = 0; i < 4; i++) {
+        Point2D loc = {10 - i, 10};
+
+        Body_push(&snake.body, Body_createPart(snake.body.length, RIGHT, loc));
+    }
+
+    Apple apple = Apple_generate(canvasWidth, canvasHeight);
+
+    render(snake, apple);
+
+    char keyCode = 0;
+    do {
+        keyCode = Console_getChar();
+
+        switch (keyCode) {
+            case BUTTON_UP: {
+                Snake_changeDirection(&snake, UP);
+                Snake_move(&snake, &apple);
+                break;
+            }
+            case BUTTON_DOWN: {
+                Snake_changeDirection(&snake, DOWN);
+                Snake_move(&snake, &apple);
+                break;
+            }
+            case BUTTON_LEFT: {
+                Snake_changeDirection(&snake, LEFT);
+                Snake_move(&snake, &apple);
+                break;
+            }
+            case BUTTON_RIGHT: {
+                Snake_changeDirection(&snake, RIGHT);
+                Snake_move(&snake, &apple);
+                break;
+            }
+        }
+
+        render(snake, apple);
+    } while (keyCode != '\n');
+
+    return 0;
+}
