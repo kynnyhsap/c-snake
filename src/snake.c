610ea6cf81c94c6a7dd7c4b3dd9831c81a1b8e2e
Andrew Pashinnik <tobirawork@gmail.com>
2018-11-07 22:43:57 +0200

    fix(snake): tryEatApple and move

--- src/snake.c
+++ src/snake.c
@@ -12,9 +12,12 @@ void Snake_render(Snake snake) {
 }
 
 void Snake_changeDirection(Snake *snake, enum Directoins newDirection) {
+    // todo: disallow change if new direction invertable to current
     snake->direction = newDirection;
 
     BodyPart head = snake->body.parts[0];
+
+    snake->body.parts[0].direction = newDirection;
 
     ChangePoint newChangePoint = {
         .point = head.location,
@@ -29,37 +32,55 @@ void Snake_tryEatApple(Snake *snake, Apple *apple) {
     BodyPart head = snake->body.parts[0];
 
     if (head.location.x == apple->location.x && head.location.y == apple->location.y) {
-        // todo: remake
-        Color greenColor = {
-            .R = 0,
-            .G = 255,
-            .B = 0,
-        };
+        BodyPart tail = snake->body.parts[snake->body.length - 1];
 
-        BodyPart newPart = Body_createPart(snake->direction, apple->location, greenColor);
+        Point2D location = tail.location;
+
+        switch (tail.direction) {
+            case UP: {
+                location.y--;
+                break;
+            }
+            case DOWN: {
+                location.y++;
+                break;
+            }
+            case LEFT: {
+                location.x++;
+                break;
+            }
+            case RIGHT: {
+                location.x--;
+                break;
+            }
+        }
+
+        BodyPart newPart = Body_createPart(snake->body.length, tail.direction, location);
 
         Body_push(&snake->body, newPart);
 
-        *apple = Apple_generate(100, 100);  // todo: dynamic size limits
+        *apple = Apple_generate(10, 30);  // todo: dynamic size limits
     }
 }
 
 void Snake_move(Snake *snake, Apple *apple) {
     for (int i = 0; i < snake->body.length; i++) {
-        BodyPart *part = &snake->body.parts[i];
-
-        switch (part->direction) {
+        switch (snake->body.parts[i].direction) {
             case UP: {
-                part->location.y++;
+                snake->body.parts[i].location.y++;
+                break;
             }
             case DOWN: {
-                part->location.y--;
+                snake->body.parts[i].location.y--;
+                break;
             }
             case RIGHT: {
-                part->location.x++;
+                snake->body.parts[i].location.x++;
+                break;
             }
             case LEFT: {
-                part->location.x--;
+                snake->body.parts[i].location.x--;
+                break;
             }
         }
 
@@ -67,11 +88,12 @@ void Snake_move(Snake *snake, Apple *apple) {
         for (int j = 0; j < snake->changePoints.length; j++) {
             ChangePoint changePoint = snake->changePoints.list[j];
 
-            if (changePoint.point.x == part->location.x && changePoint.point.y == part->location.y &&
-                changePoint.from == part->direction) {
-                part->direction = changePoint.to;
+            if (changePoint.point.x == snake->body.parts[i].location.x &&
+                changePoint.point.y == snake->body.parts[i].location.y &&
+                changePoint.from == snake->body.parts[i].direction) {
+                snake->body.parts[i].direction = changePoint.to;
 
-                if (j == 0) {
+                if (j == 0 && i == snake->body.length - 1) {
                     ChangePoints_shift(&snake->changePoints);
                 }
             }
