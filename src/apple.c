95e5cee9b6b537c3595b0394c9fd7173a32d8f4b
Andrew Pashinnik <tobirawork@gmail.com>
2018-11-07 10:18:05 +0200

    feat: add apple entity

---
+++ src/apple.c
@@ -0,0 +1,22 @@
+#include <progbase/canvas.h>
+#include <stdlib.h>
+
+#include "header.h"
+
+Apple Apple_generate(unsigned int maxX, unsigned int maxY) {
+    Apple apple = {
+        .color = {255, 0, 0},
+        .location =
+            {
+                .x = getRandomInt(0, maxX),
+                .y = getRandomInt(0, maxY),
+            },
+    };
+
+    return apple;
+}
+
+void Apple_render(Apple apple) {
+    Canvas_setColorRGB(apple.color.R, apple.color.G, apple.color.B);
+    Canvas_putPixel(apple.location.x, apple.location.y);
+}
